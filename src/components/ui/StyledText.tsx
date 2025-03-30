452133ed88e56c7e6bc437e676cee165eb4e76e9
omer-ayhan <om.ayhan247@gmail.com>
2025-03-31 00:38:43 +0300

    fix: text should have more styling props

--- src/components/ui/StyledText.tsx
+++ src/components/ui/StyledText.tsx
@@ -4,11 +4,15 @@ import {Text as RNText, TextProps as RNTextProps} from 'react-native';
 import theme from '@app/constants/theme';
 import fonts from '@app/constants/fonts';
 
+type ColorProp = ThemeColorsTypes | (string & {});
+
 interface StyledTextProps extends RNTextProps {
   variant?: TypographyVariantNames;
   fontFamily?: FontFamily;
   weight?: FontWeight;
-  color?: ThemeColorsTypes;
+  color?: ColorProp;
+  size?: number;
+  textAlign?: 'left' | 'right' | 'center';
   children: React.ReactNode;
 }
 
@@ -17,7 +21,9 @@ const StyledText = ({
   fontFamily = 'rubik',
   weight,
   color = 'dark',
+  size,
   style,
+  textAlign = 'left',
   children,
   ...rest
 }: StyledTextProps) => {
@@ -31,12 +37,26 @@ const StyledText = ({
         fontFamily: fonts[fontFamily][weight],
       }
     : {};
+  const colorValue = Object.hasOwn(theme.colors, color)
+    ? theme.colors[color as keyof typeof theme.colors]
+    : color;
   const colorStyle = {
-    color: theme.colors[color] || color,
+    color: colorValue,
   };
+  const sizeStyle = size ? {fontSize: size, lineHeight: size * 1.1} : {};
+  const textAlignStyle = textAlign ? {textAlign} : {};
 
   return (
-    <RNText style={[variantStyle, weightStyle, colorStyle, style]} {...rest}>
+    <RNText
+      style={[
+        variantStyle,
+        weightStyle,
+        colorStyle,
+        sizeStyle,
+        textAlignStyle,
+        style,
+      ]}
+      {...rest}>
       {children}
     </RNText>
   );
