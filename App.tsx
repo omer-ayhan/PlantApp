906f159fc86fd01855aa3772942cbef5e1e8de08
omer-ayhan <om.ayhan247@gmail.com>
2025-03-31 17:57:28 +0300

    perf: preload onboarding images

--- App.tsx
+++ App.tsx
@@ -1,25 +1,14 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {StatusBar, StyleSheet} from 'react-native';
 
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import BootSplash from 'react-native-bootsplash';
 
 import ReduxProvider from '@app/components/Providers/ReduxProvider';
 import RootNavigator from '@app/navigation/RootNavigator';
 import navigation from '@app/lib/navigation';
 
 function App(): React.JSX.Element {
-  useEffect(() => {
-    const init = async () => {
-      // async tasks
-    };
-
-    init().finally(async () => {
-      await BootSplash.hide({fade: true});
-    });
-  }, []);
-
   return (
     <GestureHandlerRootView style={styles.gestureHandlerRootView}>
       <ReduxProvider>
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import FastImage from 'react-native-fast-image';
+import BootSplash from 'react-native-bootsplash';
 
 import OnboardingStack from '@app/navigation/OnboardingStack';
 import MainTabs from '@app/navigation/MainTabs';
@@ -8,6 +10,18 @@ import Scan from '@app/screens/scan/Scan';
 import Paywall from '@app/screens/paywall/Paywall';
 import useOnboarding from '@app/hooks/redux/useOnboarding';
 import ROUTES from '@app/constants/routes';
+import {CDN_URL} from '@env';
+
+const ONBOARDING_IMAGES = [
+  `${CDN_URL}/onboarding/onboarding_welcome_bg.jpg`,
+  `${CDN_URL}/onboarding/welcome_plant.png`,
+  `${CDN_URL}/onboarding/brush.png`,
+  `${CDN_URL}/onboarding/info_img_1.png`,
+  `${CDN_URL}/onboarding/info_artwork.png`,
+  `${CDN_URL}/onboarding/info_iphone.png`,
+  `${CDN_URL}/onboarding/onboarding_info_bg.jpg`,
+  `${CDN_URL}/onboarding/info_leaves.jpg`,
+];
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -16,6 +30,23 @@ const RootNavigator = () => {
   const initialRouteName = hasCompletedOnboarding
     ? ROUTES.MAIN_TABS
     : ROUTES.ONBOARDING_STACK;
+
+  useEffect(() => {
+    const init = async () => {
+      if (!hasCompletedOnboarding) {
+        FastImage.preload(
+          ONBOARDING_IMAGES.map(uri => ({
+            uri,
+            priority: FastImage.priority.high,
+          })),
+        );
+      }
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+    });
+  }, [hasCompletedOnboarding]);
 
   return (
     <Stack.Navigator
